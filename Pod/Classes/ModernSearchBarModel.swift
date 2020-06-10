f5c765e076877121e52f6ffc7d863cdfcb682441
Muhammad Khusnan <muhammad.khusnan@gmail.com>
2020-06-10 19:31:16 +0700

    update mode
    menambahkan id dan link

--- Pod/Classes/ModernSearchBarModel.swift
+++ Pod/Classes/ModernSearchBarModel.swift
@@ -10,18 +10,22 @@ import UIKit
 
 public class ModernSearchBarModel: NSObject {
     
+    public var id: Int!
     public var title: String!
+    public var link: String!
     public var url: URL!
     public var imgCache: UIImage!
     
-    public init(title: String, url: String) {
+    public init(id: Int, title: String, urlImage: String, link: String) {
         super.init()
+        self.id = id
         self.title = title
-        if let newUrl = URL(string: url) {
+        self.link = link
+        if let newUrl = URL(string: urlImage) {
             self.url = newUrl
         } else {
             print("ModernSearchBarModel: Seems url is not valid...")
-            self.url = URL(string: "#")
+            self.url = URL(string: "https://ssl.gstatic.com/images/branding/product/1x/avatar_anonymous_square_512dp.png")
         }
     }
 }
